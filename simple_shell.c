f686c89e5e94fffc8d5df240943102a71e365bb2
DawitGorms <montana4828@gmail.com>
2023-12-23 00:28:43 -0800

    ayyyyyyy

--- simple_shell.c
+++ simple_shell.c
@@ -1,60 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
-#define MAX_COMMAND_LENGTH 256
+#define BUFFER_SIZE 512
+#define PROMPT "#cisfun$ "
 
-/**
- * display_prompt - Display the shell prompt.
- */
-void display_prompt(void)
+int execute_command(char *command)
 {
-    printf("#cisfun$ ");
+    pid_t pid;
+    int status;
+
+    pid = fork();
+
+    if (pid == -1)
+    {
+        perror("Fork failed");
+        return -1;
+    }
+    else if (pid == 0)
+    {
+        if (execlp(command, command, NULL) == -1)
+        {
+            fprintf(stderr, "Error: Command not found\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+    }
+
+    return (0);
 }
 
 int main(void)
 {
-    pid_t pid;
-    char *command = NULL;
-    size_t bufsize = 0;
-    int status;
+    char command[BUFFER_SIZE];
 
     while (1)
     {
-        display_prompt();
+        printf(PROMPT);
 
-        if (getline(&command, &bufsize, stdin) == -1)
-        {
-            printf("\n");
-            free(command);
-            exit(0);
-        }
+        if (fgets(command, BUFFER_SIZE, stdin) == NULL)
+            break;
 
-        command[strcspn(command, "\n")] = '\0'; /* Remove the newline character */
+        command[strcspn(command, "\n")] = '\0';  /* remove trailing newline */
 
-        pid = fork();
-
-        if (pid < 0)
-        {
-            perror("Fork failed");
-            continue;
-        }
-        else if (pid == 0)
-        {
-            if (execlp(command, command, NULL) == -1)
-            {
-                fprintf(stderr, "%s: command not found\n", command);
-                exit(1);
-            }
-        }
-        else
-        {
-            waitpid(pid, &status, 0);
-        }
+        if (execute_command(command) == -1)
+            break;
     }
 
-    return 0;
+    return (0);
 }
